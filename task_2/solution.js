b65591e3f57c8f6b68361f8906a01c644220223b
neto test dav <sergeydavidenkov@gmail.com>
2023-03-18 19:10:37 +0300

    task3 not done

--- task_2/solution.js
+++ task_2/solution.js
@@ -7,13 +7,14 @@ function calcShipping(sum, min, shipping) {
 
     let shippingSum; // создайте переменную shippingSum
 
-    if(productsSum == 0) { // если productsSum равно 0,
-        shippingSum = 0;} // то shippingSum присвоить значение 0
+    if(productsSum == 0 || productsSum >= freeShippingMinSum) { // если productsSum равно 0,
+        shippingSum = 0 ;
+    } // то shippingSum присвоить значение 0
 
-    if(productsSum >= freeShippingMinSum) { // если productsSum Больше или равна freeShippingMinSum,
-    shippingSum = 0;} // то shippingSum присвоить значение 0
+    // если productsSum Больше или равна freeShippingMinSum,
+    // то shippingSum присвоить значение 0
 
-    if(productsSum > 0 && productsSum < freeShippingMinSum) {// если productsSum больше 0 и меньше freeShippingMinSum,
+    else {// если productsSum больше 0 и меньше freeShippingMinSum,
         shippingSum = shippingPrice;}// то shippingSum присвоить значение shippingPrice
 
     // Конец решения задания №2.1.
